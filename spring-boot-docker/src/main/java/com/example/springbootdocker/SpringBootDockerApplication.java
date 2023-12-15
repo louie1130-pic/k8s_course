1aaa7569ea1e9dd626489b6bc6e15c2c6f73e0bf
omagas <omaga1130@gmail.com>
2023-12-15 09:17:00 +0800

    add spring demo

---
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,26 @@
+package com.example.springbootdocker;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+@SpringBootApplication
+public class SpringBootDockerApplication {
+ 
+        @RequestMapping("/")
+        public String home() {
+                return "Hello World!";
+        }
+
+        @RequestMapping("/one")
+        public String one() {
+                return "Hello World One";
+        }		
+ 
+        public static void main(String[] args) {
+                SpringApplication.run(SpringBootDockerApplication.class, args);
+        }
+ 
+}
