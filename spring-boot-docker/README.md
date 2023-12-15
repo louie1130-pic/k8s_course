b5e444fed7654e29aaf3196850a7c3dd9a1d3344
louie <louie1130@pic.net.tw>
2023-12-15 13:35:23 +0800

    add log and check log Success

--- spring-boot-docker/README.md
+++ spring-boot-docker/README.md
@@ -1,3 +1,5 @@
+# 一般打包手動部屬
+
 ## 建立image
 
 ### 打包
@@ -8,7 +10,11 @@ java -jar .\target\spring-boot-docker-0.0.1-SNAPSHOT.jar
 
 
 ### 透過Dockerfile建立image
-minikube image build -t spring-hellowworld-image:v1.1 -f ./Dockerfile .
+minikube image build -t spring-hellowworld-image:v1.2 -f ./Dockerfile .
+
+
+## 建立deployment
+kubectl apply -f .\deployment.yaml 
 
 
 ## 建立Service for test
@@ -18,3 +24,7 @@ kubectl expose deployment spring-boot-app --type=NodePort --port=8080
 
 ### 查詢本機(host) url和port
 minikube service spring-boot-app --url
+
+
+## 看log
+ kubectl logs -l app=spring-boot-app --all-containers
--- spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -4,23 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger;
  
 @RestController
 @SpringBootApplication
 public class SpringBootDockerApplication {
+        private static final Logger logger = Logger.getLogger(SpringBootDockerApplication.class.getName());
+
+        public static void main(String[] args) {
+                logger.info("Starting Spring Boot application");
+                SpringApplication.run(SpringBootDockerApplication.class, args);
+        }
+
  
         @RequestMapping("/")
         public String home() {
+                logger.info("Hello log!");
                 return "Hello World!";
         }
 
         @RequestMapping("/one")
         public String one() {
+                logger.info("Hello log One");
                 return "Hello World One";
         }		
  
-        public static void main(String[] args) {
-                SpringApplication.run(SpringBootDockerApplication.class, args);
-        }
+        // public static void main(String[] args) {
+        //         SpringApplication.run(SpringBootDockerApplication.class, args);
+        // }
  
 }
